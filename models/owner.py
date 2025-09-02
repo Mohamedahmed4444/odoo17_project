919b25f6737437f454435b194fcf4e0255dd40fc
Mohamedahmed4444 <ellme8753@gmail.com>
2025-09-02 23:07:12 +0300

    Update owner.py

--- models/owner.py
+++ models/owner.py
@@ -8,3 +8,7 @@ class Owner(models.Model):
     name = fields.Char(required=1)
     phone = fields.Char()
     address = fields.Char()
+    property_ids = fields.One2many(
+        comodel_name='property',
+        inverse_name='owner_id',
+    )
